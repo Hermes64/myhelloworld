d5f5c42110f3cccf377d90e634cd265fb5e1e299
Mercurio <65070556+Hermes64@users.noreply.github.com>
2021-03-05 15:52:01 +0100

    Initial commit

---
+++ src/test/java/kea/MyhelloworldApplicationTests.java
@@ -0,0 +1,13 @@
+package kea;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MyhelloworldApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
